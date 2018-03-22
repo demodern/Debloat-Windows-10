95fafd336433ef6b0abc11a532b17d24395506ef
Sam Bäumer <sba@demodern.de>
2018-03-22 18:40:41 +0100

    added all in one script

---
+++ launch-basic-scripts.ps1
@@ -0,0 +1,7 @@
+& powershell.exe -File scripts\block-telemetry.ps1
+& powershell.exe -File scripts\disable-services.ps1
+& powershell.exe -File scripts\fix-privacy-settings.ps1
+& powershell.exe -File scripts\optimize-windows-update.ps1
+& powershell.exe -File scripts\remove-default-apps.ps1
+& powershell.exe -File scripts\remove-onedrive.ps1
+& powershell.exe -File utils\disable-scheduled-tasks.ps1
